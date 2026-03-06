f550881e349be32c01a544614f4eb6d6e63b6ca4
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2026-03-06 12:32:36 -0300

    Init backend
    Project Type: Maven
    Java: 25
    Spring Boot: 4.0.3
    Dependencies: web,data-jpa,postgresql,validation,lombok

---
+++ backend/src/main/java/com/calevin/deck_admin/DecksAdminApplication.java
@@ -0,0 +1,13 @@
+package com.calevin.deck_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecksAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecksAdminApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/calevin/deck_admin/DecksAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calevin.deck_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecksAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
